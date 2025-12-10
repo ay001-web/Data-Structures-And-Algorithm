bab482ce1143ac897d9e2d2519987c5fb39efcc5
ay001-web <ayushyadav2250@gmail.com>
2025-12-10 16:54:35 +0530

    Comments

---
+++ Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+    public static void main(String args[]) {
+        // Code for comments with forward slash
+        /*
+        Hello world
+        Bye world
+        */
+    }
+    
+}
