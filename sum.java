e227d800968db09dd8ab02e7abb74e9ccf8ec77b
ay001-web <ayushyadav2250@gmail.com>
2025-12-10 16:36:26 +0530

    sum

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String args[]) {
+        // Code to calculate sum
+        int a = 10;
+        int b= 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    
+}
