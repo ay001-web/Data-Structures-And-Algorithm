f23d89b84e0f1dae3d88c6b9bfd70dff36e2a743
ay001-web <ayushyadav2250@gmail.com>
2025-12-10 16:42:45 +0530

    Sum

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum {
     public static void main(String args[]) {
-        // Code to calculate sum
+        // Code to calculate sum of numbers
         int a = 10;
         int b= 5;
         int sum = a+b;
