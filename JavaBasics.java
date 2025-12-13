a0c977469f46b914aae89a5b2451d7bd90d213d9
ay001-web <ayushyadav2250@gmail.com>
2025-12-13 13:59:40 +0530

    Variables

--- JavaBasics.java
+++
@@ -1,8 +0,0 @@
-public class JavaBasics {
-    public static void main(String args[]) {
-        System.out.println("Hello Ayush!"); //Line ka space
-        System.out.println("Hello Ayush!"); //Line ka space
-        System.out.println("Hello Ayush!");//Line ka space
-    }
-    
-}
