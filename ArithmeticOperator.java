dbea2ac5ccca8f921a6b2a5798cf10db36576faf
ay001-web <ayushyadav2250@gmail.com>
2025-12-13 14:47:05 +0530

    Artihmetic Operator

---
+++ ArithmeticOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArithmeticOperator {
+    public static void main(String args[]) {
+        int A = 10;
+        int B = 5;
+        System.out.println("Add:" + (A+B));
+        System.out.println("subtract:" + (A-B));
+        System.out.println("Multiplication:" + (A*B));
+        System.out.println("Division:" + (A/B));
+        System.out.println("Modulus:" + (A%B));
+    }
+}
