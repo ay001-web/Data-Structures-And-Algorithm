7d25cd3a62117dd1ae4d0eabc52a0ac41309f1c3
ay001-web <ayushyadav2250@gmail.com>
2025-12-13 14:05:44 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String args[]) {
+        // code for variables
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name="Tony stark";
+        System.out.println(name);
+
+        a=b;
+        System.out.println(a);
+    }
+    
+}
