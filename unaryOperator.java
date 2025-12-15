846a6529ebd5b8ec5ee527b1a1fa8c8aad791567
ay001-web <ayushyadav2250@gmail.com>
2025-12-15 13:21:19 +0530

    Unary operator

---
+++ unaryOperator.java
@@ -0,0 +1,32 @@
+public class unaryOperator {
+    public static void main (String args[]) {
+        /*  
+        Pre increment
+        int a = 10;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        */
+        /*
+        Post increment
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+        */
+        /* 
+        Pre decrement
+        int a = 10;
+        int b =--a;
+        System.out.println(a);
+        System.out.println(b);
+        */
+        /* 
+        Post decrement
+        int a = 10;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b);
+        */
+    }
+}
