5fcffdde7b7e0f7d45dbc603ceb3c21e70712512
ay001-web <ayushyadav2250@gmail.com>
2025-12-15 13:59:01 +0530

    Logical Operator

---
+++ LogicalOperator.java
@@ -0,0 +1,22 @@
+public class LogicalOperator {
+    public static void main(String args[]) {
+        /* 
+        Logical AND
+        System.out.println((3>2)&&(5>0));
+        */
+
+
+       /* 
+        Logical OR
+        System.out.println((3<2)||(3>2));
+        */
+
+
+        /* 
+        Logical NOT
+       System.out.println(!(3>2));
+
+       */
+    }
+    
+}
