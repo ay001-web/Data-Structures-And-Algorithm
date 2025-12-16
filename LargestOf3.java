6eefac34e5a4c03fb80ae438faac859e137fd1c7
ay001-web <ayushyadav2250@gmail.com>
2025-12-16 14:48:54 +0530

    Calculating Largest of 3 numbers

---
+++ LargestOf3.java
@@ -0,0 +1,19 @@
+public class LargestOf3 {
+    public static void main(String args[]) {
+        int A = 1;
+        int B = 3;
+        int C = 6;
+        if(A>=B && A>=C) {
+            System.out.println("A is Largest");
+        }
+        else if(B>=C) {
+            System.out.println("B is Largest");
+
+        }
+        else {
+            System.out.println("C is Largest");
+        }
+
+    }
+    
+}
