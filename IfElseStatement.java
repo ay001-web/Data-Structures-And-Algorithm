1b7398b8dc5b1468406911b9c6b5355c2001d040
ay001-web <ayushyadav2250@gmail.com>
2025-12-16 13:56:01 +0530

    If Else Conditional Statement

---
+++ IfElseStatement.java
@@ -0,0 +1,14 @@
+public class IfElseStatement {
+    public static void main(String args[]) {
+        int age = 16;
+        if(age>=18) {
+            System.out.println("Adult:drive,vote");
+        }
+        if(age>13 && age<18) {
+            System.out.println("Teenager");
+        }
+        else {
+            System.out.println("Not adult");
+        }
+    }
+}
