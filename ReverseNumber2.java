9a66b8a6c761152f1f744cb231b83cf834505929
ay001-web <ayushyadav2250@gmail.com>
2025-12-18 15:28:32 +0530

    Reversing the given number using while loop

---
+++ ReverseNumber2.java
@@ -0,0 +1,14 @@
+public class ReverseNumber2 {
+    public static void main(String args[]) {
+        int n = 22456;
+        int rev = 0;
+        while(n>0) {
+            int LastDigit = n % 10;
+          rev = (rev*10) + LastDigit;
+            n/=10;
+            
+        }
+        System.out.println(rev);
+    }
+    
+}
