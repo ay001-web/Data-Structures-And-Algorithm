d6076b42704183df1a78aa6c4f0bace903291842
ay001-web <ayushyadav2250@gmail.com>
2025-12-18 15:05:16 +0530

    Printing square Pattern

---
+++ PrintSquare.java
@@ -0,0 +1,8 @@
+public class PrintSquare {
+    public static void main(String args[]) {
+        for(int Line = 1; Line<=4; Line++) {
+            System.out.println("****");
+        }
+    }
+    
+}
