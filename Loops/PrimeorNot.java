87d189f2bb727fb59c8264f530934f10ca987675
ay001-web <ayushyadav2250@gmail.com>
2025-12-19 15:01:54 +0530

    Checking If The Number Is Prime Or Not

--- Loops/PrimeorNot.java
+++ Loops/PrimeorNot.java
@@ -10,7 +10,7 @@ public class PrimeorNot {
         }
         else {
             boolean isPrime = true;
-        for(int i = 2; i<=n-1; i++ ) {
+        for(int i = 2; i<=Math.sqrt(n); i++ ) {
             if(n%i==0) {
                 isPrime = false;
             }
