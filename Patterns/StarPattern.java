a855bf1ba21ab91e8483b023bd95890a1fbd33bc
ay001-web <ayushyadav2250@gmail.com>
2025-12-20 13:54:31 +0530

    Printing Star Pattern Using Nested Loop

--- Patterns/StarPattern.java
+++ Patterns/StarPattern.java
@@ -2,6 +2,7 @@ package Patterns;
 public class StarPattern {
     public static void main(String args[]) {
         for(int line = 1; line<=4 ; line++) {
+            // One Line
             for( int star = 1; star<=line ; star++) {
                 System.out.print("*");
             }
