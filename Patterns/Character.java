b2afd2c32a79f2e2a918b1e238f4ba20fdbc8a0e
ay001-web <ayushyadav2250@gmail.com>
2025-12-20 14:24:00 +0530

    Printing Character Pattern Using Nested Loop

---
+++ Patterns/Character.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Character {
+    public static void main(String args[]) {
+        int n = 4;
+        char ch = 'A';
+        //Outer loop
+        for(int line = 1;line<=n;line++) {
+            //Inner loop
+            for(int chars = 1 ; chars<=line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
