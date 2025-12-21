b967efa5b220266617d7c998161cdbc9fa1022d7
ay001-web <ayushyadav2250@gmail.com>
2025-12-21 15:12:22 +0530

    Syntax of writing a Function

---
+++ Functions/Syntax.java
@@ -0,0 +1,12 @@
+package Functions;
+public class Syntax {
+    public static void printHelloWorld(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+    }
+    public static void main(String args[]) {
+        printHelloWorld(); //Function call
+    }
+}
