2f2793f70cde9f1c01f49ad1b1b385ff2ed77d7a
ay001-web <ayushyadav2250@gmail.com>
2026-01-06 23:58:53 +0530

    Butterfly Pattern

---
+++ Patterns(Part-2)/ButterflyPattern.java
@@ -0,0 +1,30 @@
+public class ButterflyPattern {
+    public static void butterfly (int n) {
+        for(int i = 1;i<=n;i++) {
+            for(int j = 1;j<=i;j++) {
+                System.out.print("*");
+
+            }
+            for(int j = 1;j<=2*(n=1);j++) {
+                System.out.print("");
+            }
+            for(int j = 1;j<=i;j++) {
+                System.out.print("*");
+            }
+
+        }
+        for(int i=n;i>=1;i--) {
+            for(int j = 1;j<=2*(n=1);j++) {
+                System.out.print("");
+            }
+            for(int j = 1;j<=i;j++) {
+                System.out.print("*");
+            }
+        }
+    }
+    public static void  main(String args[]) {
+        butterfly(4);
+    }
+    
+    
+}
