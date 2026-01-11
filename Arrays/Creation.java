d31193e0201e01724c4b5bd414e9d6590a24ad55
ay001-web <ayushyadav2250@gmail.com>
2026-01-11 23:31:57 +0530

    Creation of Array

--- Arrays/Creation.java
+++ Arrays/Creation.java
@@ -1,5 +1,12 @@
 package Arrays;
+import java.util.*;
 
 public class Creation {
+    public static void main(String args[]) {
+        int marks[] = new int[50];
+        int numbers[] = {1,2,3,4};
+        int moreNumbers[] = {4,5,6};
+
+    }
     
 }
