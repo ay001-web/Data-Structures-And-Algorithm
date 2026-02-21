9b270bba70a52323a1c2225f91edb51b08d91278
ay001-web <ayushyadav2250@gmail.com>
2026-02-22 00:04:24 +0530

    Linear Search

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -7,19 +7,16 @@ public class LinearSearch {
             }
         }
         return -1;
-
     }
-    public static void main(String args[]) {
-        int numbers[] = {2,4,6,8,10,12,14,16};
-        int key = 10;
-        int index = LinearSearch(numbers,key);
-        if(index==-1) {
-            System.out.println("NOT Found");
-        }
-        else {
-            System.out.println("Key is at index : " + index);
-        }
-
+   public static void main(String args[]) {
+    int numbers[] = {2,4,6,8,10,12,14,16};
+    int key = 10;
+    int index = LinearSearch(numbers,key);
+    if(index== -1) {
+        System.out.println("NOT FOUND");
     }
-    
+    else {
+        System.out.println("Key is at index " +index);
+    }
+   }
 }
